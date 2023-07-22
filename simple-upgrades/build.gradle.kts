f0f89f123e539ab12487ebdff9a2637be6a3e507
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2023-07-22 15:42:14 +0200

    Fix Simple-Upgrades and Vanilla-Hammers addon names

--- simple-upgrades/build.gradle.kts
+++ simple-upgrades/build.gradle.kts
@@ -2,5 +2,6 @@ version = "1.1"
 
 addon {
     main.set("xyz.xenondevs.nova.addon.simpleupgrades.SimpleUpgrades")
+    name.set("Simple-Upgrades")
     authors.addAll("StudioCode")
 }
--- vanilla-hammers/build.gradle.kts
+++ vanilla-hammers/build.gradle.kts
@@ -2,5 +2,6 @@ version = "1.2"
 
 addon {
     main.set("xyz.xenondevs.nova.addon.vanillahammers.VanillaHammers")
+    name.set("Vanilla-Hammers")
     authors.addAll("StudioCode")
 }
