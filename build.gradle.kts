e318ba5cfca9fbe503a2a6f6d1aec9fc106bd7b9
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2023-08-27 11:21:21 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,37 +32,39 @@ subprojects {
     apply(plugin = rootProject.libs.plugins.nova.get().pluginId)
     apply(plugin = rootProject.libs.plugins.paperweight.get().pluginId)
     apply(plugin = rootProject.libs.plugins.stringremapper.get().pluginId)
-
+    
     repositories { configureRepositories() }
     dependencies { configureDependencies() }
-
+    
     addon {
         id.set(this@subprojects.name)
         name.set(this@subprojects.name.capitalized())
         novaVersion.set(rootProject.libs.versions.nova)
     }
-
+    
     remapStrings {
         remapGoal.set(if (mojangMapped) "mojang" else "spigot")
         gameVersion.set(rootProject.libs.versions.paper.get().substringBefore("-"))
     }
-
+    
     tasks {
+        val buildDir = project.layout.buildDirectory.get().asFile
+        val outDir = (project.findProperty("outDir") as? String)?.let(::File) ?: buildDir
+        
         register<Copy>("addonJar") {
             group = "build"
-            dependsOn("addon")
             if (mojangMapped) {
                 dependsOn("jar")
-                from(File(project.buildDir, "libs/${project.name}-${project.version}-dev.jar"))
+                from(File(buildDir, "libs/${project.name}-${project.version}-dev.jar"))
             } else {
                 dependsOn("reobfJar")
-                from(File(project.buildDir, "libs/${project.name}-${project.version}.jar"))
+                from(File(buildDir, "libs/${project.name}-${project.version}.jar"))
             }
             
-            into((project.findProperty("outDir") as? String)?.let(::File) ?: project.buildDir)
-            rename { "${addon.get().addonName.get()}-${project.version}.jar" }
+            into(outDir)
+            rename { "${addonMetadata.get().addonName.get()}-${project.version}.jar" }
         }
-
+        
         withType<KotlinCompile> {
             kotlinOptions {
                 jvmTarget = "17"
