2ea7e0463113090855937967e7767977ed24ff5a
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2024-08-27 18:11:27 +0200

    Allow creative pick block on wind turbine section

--- machines/src/main/kotlin/xyz/xenondevs/nova/addon/machines/block/WindTurbineSectionBehavior.kt
+++ machines/src/main/kotlin/xyz/xenondevs/nova/addon/machines/block/WindTurbineSectionBehavior.kt
@@ -1,7 +1,9 @@
 package xyz.xenondevs.nova.addon.machines.block
 
+import org.bukkit.inventory.ItemStack
 import xyz.xenondevs.nova.addon.machines.registry.BlockStateProperties
 import xyz.xenondevs.nova.addon.machines.registry.ContextParamTypes
+import xyz.xenondevs.nova.addon.machines.registry.Items
 import xyz.xenondevs.nova.context.Context
 import xyz.xenondevs.nova.context.intention.DefaultContextIntentions.BlockBreak
 import xyz.xenondevs.nova.context.intention.DefaultContextIntentions.BlockInteract
@@ -44,6 +46,10 @@ object WindTurbineSectionBehavior : BlockBehavior {
                 .build()
             BlockUtils.breakBlockNaturally(sectionCtx)
         }
+    }
+    
+    override fun pickBlockCreative(pos: BlockPos, state: NovaBlockState, ctx: Context<BlockInteract>): ItemStack {
+        return Items.WIND_TURBINE.createItemStack()
     }
     
 }
