afd5d22b19f9141549709332230584f768be1f39
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2024-12-18 15:43:45 +0100

    Update Nova

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("xyz.xenondevs.nova:catalog:0.18-alpha.6")
+            from("xyz.xenondevs.nova:catalog:0.18-alpha.8")
         }
     }
 }
