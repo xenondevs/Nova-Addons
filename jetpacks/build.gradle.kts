5480e96e2a6f32dcda50cadc99a409422eaa0612
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2025-05-29 19:02:03 +0200

    Bump versions

--- jetpacks/build.gradle.kts
+++ jetpacks/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("addons.common-conventions")
 }
 
-version = "0.2-alpha.2"
+version = "0.2-RC.1"
 
 addon {
     name = "Jetpacks"
--- simple-upgrades/build.gradle.kts
+++ simple-upgrades/build.gradle.kts
@@ -1,4 +1,4 @@
-version = "1.6-alpha.1"
+version = "1.6-RC.1"
 
 plugins {
     id("addons.common-conventions")
